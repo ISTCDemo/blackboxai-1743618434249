970af8a4082bf0285bb1963dd298249d5b6c6b27
ISTCDemo <saanjhdev@gmail.com>
2025-04-03 01:04:55 +0530

    Add ClassPlusClone/app/src/main/java/com/classplus/CoursesFragment.java 1743622506155

---
+++ ClassPlusClone/app/src/main/java/com/classplus/CoursesFragment.java
@@ -0,0 +1,16 @@
+package com.classplus;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import androidx.fragment.app.Fragment;
+
+public class CoursesFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_courses, container, false);
+    }
+}
